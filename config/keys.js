85cb8fb94507e11659d4cf569ca1a59553b6b560
king dave <bigthinzcount@gmail.com>
2020-10-20 17:28:38 +0100

    setting up backend api

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://localhost:27017/mern_shopping'
+        // mongoURI: "mongodb+srv://dave:cainandabel4o@cluster0.yeihc.mongodb.net/mern_shopping?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,5 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const item = require('./routes/api/items')
 
 const app = express()
+
+//bodyParser middleware
+app.use(bodyParser.json())
+
+//DB
+const db = require('./config/keys').mongoURI
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(con => {
+    console.log('database connected')
+}).catch(err => {
+    console.log(err)
+})
+
+
+app.use('/api/items', item)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running on ${port}`))
