3b42d60199a423f495722498303ea39d6a809a10
king dave <bigthinzcount@gmail.com>
2020-10-23 23:40:08 +0100

    client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import AppNavbar from './components/AppNavbar'
+
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > Hello < /h1> <
-        /div>
+    return ( 
+        <div className = "App" >
+        <AppNavbar/> 
+        </div>
     );
 }
 
